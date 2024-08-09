2f2d9da0187d5e2c4b398b3df1730c4ce1f1a42e
sergi <sergifusterdura@gmail.com>
2024-08-09 02:31:29 +0200

    feat: naive csv to html table tool

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from src.tools.tools import *
 
 drawer = "data/files"
 
-def main():
+def write():
     args = sys.argv[1:]
 
     data = parse_inputs(args)
 
     write_csv(data, os.path.join(drawer, f'{today_date()}.csv'))
 
+def read():
+
+    filename = sys.argv[1]
+
+    html = to_html(os.path.join(drawer, f'{filename}.csv'))
+
+    with open(os.path.join(drawer, f'{today_date()}.html'), 'w') as file:
+        file.write(html)
 if __name__ == "__main__":
-    main()
+    read()
--- src/tools/tools.py
+++ src/tools/tools.py
@@ -1,9 +1,22 @@
-import sys, os, csv
+import sys, os, csv, pandas as pd
 from datetime import datetime
 
-def write_csv(data, filename):
+def to_html(filepath):
+    data = read_csv(filepath)
+    html = data.to_html()
+    return html
 
-    with open(os.path.join(filename), mode='w', newline="") as file:
+def read_csv(filepath):
+    try:
+        data = pd.read_csv(filepath)
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit(1)
+    return data
+
+def write_csv(data, filepath):
+
+    with open(os.path.join(filepath), mode='w', newline="") as file:
         writer = csv.writer(file)
         writer.writerows(data)
 
